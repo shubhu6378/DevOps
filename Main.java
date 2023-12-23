860ea55861d753c428345bf5c5f8213a3eefd38f
Shubham Kumar <125904884+shubhu6378@users.noreply.github.com>
2023-12-23 14:49:47 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main{
     public static void main(String args[]){  
     	System.out.println("Hello Java");  
     	System.out.println("Hello Shubham");
+        System.out.println("Hello Shubham, Jenkins Pipeline running fine");
     }  
 }  
